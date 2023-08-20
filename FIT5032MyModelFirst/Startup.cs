0a0b62c1d18b8debb81db001f64783026aff4e7a
Kalibur233 <mlii0076@student.monash.edu>
2023-08-20 18:27:18 +1000

    Add project files.

---
+++ FIT5032MyModelFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIT5032MyModelFirst.Startup))]
+namespace FIT5032MyModelFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
